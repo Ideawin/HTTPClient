0102873121bb4da083831473a6bad7a683aad2cc
Ideawin-Bunthy Koun <ideawinkoun@gmail.com>
2017-10-02 15:11:24 -0400

    Checking for empty responses in HTTPResponse

--- HTTPClient/src/HTTPResponse.java
+++ HTTPClient/src/HTTPResponse.java
@@ -6,7 +6,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class HTTPResponse {
-	
+
 	// Attributes
 	private String response = "";
 	private SocketChannel socket;
@@ -17,7 +17,7 @@ public class HTTPResponse {
 	 * Default constructor
 	 */
 	public HTTPResponse() {}
-	
+
 	/**
 	 * Constructor with a socket passed as a parameter
 	 * @param socket
@@ -25,7 +25,7 @@ public class HTTPResponse {
 	public HTTPResponse(SocketChannel socket) {
 		this.socket = socket;
 	}
-	
+
 	/**
 	 * Method to query parameters of the request
 	 * @param buf: the buffer containing the response
@@ -37,7 +37,7 @@ public class HTTPResponse {
 		String[] strArr = response.split("\r\n");
 		return strArr[strArr.length - 1]; // must only take the bottom half
 	}
-	
+
 	/**
 	 * Method to get the full server's response
 	 * @param buf: the buffer containing the response
@@ -45,16 +45,16 @@ public class HTTPResponse {
 	 * @throws IOException
 	 */
 	public String getFullResponse(ByteBuffer buf) throws IOException {
-    	Charset utf8 = StandardCharsets.UTF_8;
-    	while ((socket.read(buf) != -1)) {
-		    buf.flip();
-		    response += utf8.decode(buf);
-		    buf.clear();
+		Charset utf8 = StandardCharsets.UTF_8;
+		while ((socket.read(buf) != -1)) {
+			buf.flip();
+			response += utf8.decode(buf);
+			buf.clear();
 		}
-    	checkForRedirection();
-    	return response;
+		checkForRedirection();
+		return response;
 	}
-	
+
 	/**
 	 * Method to get the status code of the response's status line
 	 * @return
@@ -63,23 +63,24 @@ public class HTTPResponse {
 		String statusCode = statusLine.substring(9, 10);
 		return statusCode;
 	}
-	
+
 	/**
 	 * Method to obtain the url of the redirected request
 	 */
 	public void checkForRedirection() {
-		statusLine = response.substring(0, response.indexOf("\n"));
-		String statusCode = statusLine.substring(9, 10);
-		if (statusCode.equals("3")) {
-    		String[] responseArray = response.split("\r\n");
-    		for (int i = 0; i<responseArray.length;i++) {
-    			if (responseArray[i].length() > 9 && responseArray[i].substring(0, 8).equals("Location")) {
-    				url = responseArray[i].substring(10);
-    			}
-    		}
+		if (!response.isEmpty()) {
+			statusLine = response.substring(0, response.indexOf("\n"));
+			String statusCode = statusLine.substring(9, 10);
+			if (statusCode.equals("3")) {
+				String[] responseArray = response.split("\r\n");
+				for (int i = 0; i<responseArray.length;i++) {
+					if (responseArray[i].length() > 9 && responseArray[i].substring(0, 8).equals("Location")) {
+						url = responseArray[i].substring(10);
+					}
+				}
+			}
 		}
-	
 	}
-	
-	
+
+
 }
